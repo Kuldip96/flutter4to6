7655724b3cbe4da8effaaef5387547bbb77e7f83
unknown <kuldeep.skillqode@gmail.com>
2024-09-14 16:40:39 +0530

    14-09-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demoapp/container_screen.dart';
 import 'package:demoapp/scafflod_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ScafflodScreen(),
+      home: const ContainerScreen(),
     );
   }
 }
