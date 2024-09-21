21e0ccd62c18e436f6c89d581a3f8f29c1c7117f
unknown <kuldeep.skillqode@gmail.com>
2024-09-21 16:51:20 +0530

    font famaly

--- lib/row_column.dart
+++ lib/row_column.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RowColumn extends StatefulWidget {
   const RowColumn({super.key});
@@ -10,66 +11,73 @@ class RowColumn extends StatefulWidget {
 class _RowColumnState extends State<RowColumn> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // Row(
-          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //   children: [
-          // Container(
-          //   height: 50,
-          //   width: 100,
-          //   color: Colors.green,
-          // ),
-          // Container(
-          //   height: 50,
-          //   width: 100,
-          //   color: Colors.yellow,
-          // ),
-          // Container(
-          //   height: 50,
-          //   width: 100,
-          //   color: Colors.blue,
-          // ),
-          //   ],
-          // ),
-          Container(
-            height: 50,
-            width: 100,
-            color: Colors.green,
-            child: Text(
-              textAlign: TextAlign.center,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              'da',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                //  letterSpacing: 5,
+    return SafeArea(
+      // bottom: false,
+      // left: false,
+      // right: false,
+      // top: true,
+      // minimum: EdgeInsets.all(10),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: [
+            // Container(
+            //   height: 50,
+            //   width: 100,
+            //   color: Colors.green,
+            // ),
+            // Container(
+            //   height: 50,
+            //   width: 100,
+            //   color: Colors.yellow,
+            // ),
+            // Container(
+            //   height: 50,
+            //   width: 100,
+            //   color: Colors.blue,
+            // ),
+            //   ],
+            // ),
+            Container(
+              height: 50,
+              width: 100,
+              color: Colors.green,
+              child: Text(
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                'da',
+                style: GoogleFonts.poppins(
+                  // fontFamily: 'JacquardaBastarda9',
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  //  letterSpacing: 5,
+                ),
               ),
             ),
-          ),
-          Container(
-            height: 50,
-            width: 100,
-            color: Colors.yellow,
-            child: Icon(
-              Icons.abc,
-              size: 40,
-              
+            Container(
+              height: 50,
+              width: 100,
+              color: Colors.yellow,
+              child: const Icon(
+                Icons.abc,
+                size: 40,
+              ),
             ),
-          ),
-          Container(
-            height: 50,
-            width: 100,
-            color: Colors.blue,
-          ),
-        ],
+            Container(
+              height: 50,
+              width: 100,
+              color: Colors.blue,
+            ),
+          ],
+        ),
       ),
     );
   }
