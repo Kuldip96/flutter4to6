978fa4dfb4dd58334fb87945e0636a8400c4310d
unknown <kuldeep.skillqode@gmail.com>
2024-11-21 16:24:04 +0530

    gridview count

--- lib/lacture11.dart
+++ lib/lacture11.dart
@@ -27,23 +27,70 @@ class _Lacture11State extends State<Lacture11> {
       //       },
       //       itemCount: 5),
       // );
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1,
-          crossAxisSpacing: 10,
-          mainAxisExtent: 200,
-          mainAxisSpacing: 10,
-        ),
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Card(
-            color: Colors.pink,
-            child: Center(
-              child: Text(index.toString()),
-            ),
-          );
-        },
+      // body: GridView.builder(
+      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //     crossAxisCount: 2,
+      //     childAspectRatio: 1,
+      //     crossAxisSpacing: 10,
+      //     mainAxisExtent: 200,
+      //     mainAxisSpacing: 10,
+      //   ),
+      //   itemCount: 10,
+      //   itemBuilder: (context, index) {
+      //     return Card(
+      //       color: Colors.pink,
+      //       child: Center(
+      //         child: Text(index.toString()),
+      //       ),
+      //     );
+      //   },
+      // ),
+
+      // body: GridView.count(
+      //   crossAxisCount: 2,
+      //   crossAxisSpacing: 10,
+      //   mainAxisSpacing: 10,
+      //   padding: EdgeInsets.all(10),
+      //   children: [
+      // Container(
+      //   color: Colors.yellow,
+      // ),
+      // Container(
+      //   color: Colors.black,
+      // ),
+      // Container(
+      //   color: Colors.brown,
+      // ),
+      // Container(
+      //   color: Colors.grey,
+      // ),
+      // Container(
+      //   color: Colors.green,
+      // ),
+      //   ],
+      // ),
+
+      body: GridView.extent(
+        maxCrossAxisExtent: 200,
+        // reverse: true,
+        
+        children: [
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.black,
+          ),
+          Container(
+            color: Colors.brown,
+          ),
+          Container(
+            color: Colors.grey,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+        ],
       ),
     );
   }
