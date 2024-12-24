436a9e62c2a1636c196b7ef091ee921f6409fab3
Kuldip96 <kuldeep.skillqode@gmail.com>
2024-12-24 16:40:17 +0530

    24-12-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:demoapp/lacture01.dart';
 import 'package:demoapp/lacture02.dart';
+import 'package:demoapp/lacture15.dart';
 import 'package:demoapp/mediaquey.dart';
 
 import 'package:flutter/material.dart';
@@ -21,11 +22,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: '/',
+      initialRoute: 'lacture15',
       routes: {
         '/': (context) => Lacture01(),
         'lacture2': (context) => Lacture02(),
-        
+        'lacture15': (context) => Lacture15(),
       },
       // home: Lacture01(),
     );
